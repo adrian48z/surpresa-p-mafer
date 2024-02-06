67bca3fe8e16ea44a56f9afc750a56b0b642f079
adrian48z <159115858+adrian48z@users.noreply.github.com>
2024-02-06 05:40:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({
-        duration: 800,
-        easing: 'ease-in-out',
-    });
-
-    const contactForm = document.getElementById('contact-form');
-    const formError = document.getElementById('form-error');
-    const formSuccess = document.getElementById('form-success');
-
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const name = document.getElementById('name').value.trim();
-        const email = document.getElementById('email').value.trim();
-        const message = document.getElementById('message').value.trim();
-
-        if (name === '' || email === '' || message === '') {
-            formError.textContent = 'Por favor, preencha todos os campos.';
-            formSuccess.textContent = '';
-        } else {
-            setTimeout(function() {
-                formError.textContent = '';
-                formSuccess.textContent = 'Obrigado por nos contatar!';
-                contactForm.reset();
-            }, 1000);
-        }
-    });
-});
+    const sections = document.querySelectorAll('.section');
+    const form = document.getElementById('contact-form');
+    const formError = document
